71374629c337ebb5ca5525f86781c312d6f4986d
wassim rim <wassim_rim@hotmail.fr>
2019-12-10 21:34:41 +0100

    first commit

---
+++ src/main/java/com/foruminfo/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.foruminfo.demo.controller;
+
+
+import com.foruminfo.demo.exception.ResourceNotFoundException;
+import com.foruminfo.demo.model.User;
+import com.foruminfo.demo.repository.UserRepository;
+import com.foruminfo.demo.security.CurrentUser;
+import com.foruminfo.demo.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
---
+++ src/main/java/com/foruminfo/demo/model/Domain.java
@@ -0,0 +1,11 @@
+package com.foruminfo.demo.model;
+
+public enum Domain {
+
+    CPI,
+    TIC,
+    GLSI,
+    DSEN,
+    DMWM,
+    SSIR
+}
